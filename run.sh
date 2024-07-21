826c4796d8194f5b1498b890f92f467355e2bffb
Nghia Pham <nghia.pq77@gmail.com>
2024-07-21 11:21:55 +0700

    Add --force option for run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,42 @@
 #!/bin/sh
 
+link(){
+    ln -s "$1" "$2" && echo "- Symlinked $1 to $2"
+}
+
 symlink(){
-    if [ -d "$2" ] || [ -f "$2" ] || [ -L "$2" ]; then
-        echo "x $2 already existed, please remove first"
+    if [ $FORCE = 1 ]; then
+        [ -L "$2" ] && rm "$2" && printf "\t-- Removed symlink %s\n" "$2"
+        [ -d "$2" ] && rm -r "$2" && printf "\t-- Removed directory %s\n" "$2"
+        link "$1" "$2"
     else
-        ln -s "$1" "$2"
-        echo "Symlinked $1 to $2"
+        if [ -d "$2" ] || [ -f "$2" ] || [ -L "$2" ]; then
+            echo "x $2 already existed, please remove it first or use --force option"
+        else
+            link "$1" "$2"
+        fi
     fi
 }
+
+FORCE=0
+while :; do
+    case $1 in
+        -f|--force)
+            FORCE=1
+            echo "Running in FORCE mode"
+            ;;
+        --) # End of all options.
+            shift
+            break
+            ;;
+        -?*)
+            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+            ;;
+        *)  # Default case: If no more options then break out of the loop.
+            break
+    esac
+    shift
+done
 
 # .local/bin
 BIN_SRC_PATH="$PWD/.local/bin"
